5f415e7f0daa58245461027ed1a3e15db4a99c10
menna <menna@menna-PC>
2016-05-05 13:41:33 +0200

    Model Module (Daos,Pojos)

---
+++ Development/Web/Etbo5lyApp/Etbo5ly-Model/src/main/java/com/iti/jet/gp/etbo5ly/model/dao/interfaces/CookStatusDao.java
@@ -0,0 +1,8 @@
+package com.iti.jet.gp.etbo5ly.model.dao.interfaces;
+
+import com.iti.jet.gp.etbo5ly.model.pojos.CookStatus;
+import com.iti.jet.gp.etbo5ly.model.generic.dao.GenericDao;
+
+public interface CookStatusDao extends GenericDao<CookStatus>{
+
+}
