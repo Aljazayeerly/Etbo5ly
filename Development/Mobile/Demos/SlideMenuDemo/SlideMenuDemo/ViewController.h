87bdb5d130c4b6c3cdd0a15b0a7b3119bd906adf
Aljazayeerly <mohammedaljazayeerly@gmail.com>
2016-05-21 12:17:09 +0200

    Add mobile project

---
+++ Development/Mobile/Demos/SlideMenuDemo/SlideMenuDemo/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  SlideMenuDemo
+//
+//  Created by ITI on 5/18/16.
+//  Copyright (c) 2016 JETS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak,nonatomic) IBOutlet UIBarButtonItem *barButtonItem;
+
+@end
+
---
+++ Development/Web/Etbo5lyApp/Etbo5ly-Service/src/main/java/com/iti/jet/gp/etbo5ly/service/AdminService.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.iti.jet.gp.etbo5ly.service;
+
+import com.iti.jet.gp.etbo5ly.model.pojo.Category;
+import java.util.List;
+
+/**
+ *
+ * @author AlJazayeerly
+ */
+public interface TestService {
+    List<Category> getAllCategories();
+}
